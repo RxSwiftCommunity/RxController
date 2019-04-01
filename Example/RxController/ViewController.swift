749b957928ce391fbca8ec80ee270711ce8ec267
Meng Li <lm2343635@126.com>
2019-04-01 11:19:28 +0900

    Remove storyboard.

--- Example/RxController/ViewController.swift
+++ Example/RxController/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  RxController
 //
-//  Created by lm2343635 on 04/01/2019.
-//  Copyright (c) 2019 lm2343635. All rights reserved.
+//  Created by Meng Li on 04/01/2019.
+//  Copyright (c) 2019 MuShare. All rights reserved.
 //
 
 import UIKit
@@ -12,13 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .white
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 }
-

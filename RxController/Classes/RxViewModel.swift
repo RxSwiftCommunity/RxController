d678ccbc6cf6bfecc1c8418b9d16876dd5751284
Meng Li <lm2343635@126.com>
2020-05-01 03:32:50 +0900

    Remove unused imports

--- RxController/Classes/RxViewModel.swift
+++ RxController/Classes/RxViewModel.swift
@@ -3,7 +3,7 @@
 //  RxController
 //
 //  Created by Meng Li on 04/09/2019.
-//  Copyright (c) 2019 XFLAG. All rights reserved.
+//  Copyright (c) 2019 MuShare. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,7 +23,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-import Foundation
 import RxSwift
 import RxCocoa
 import RxFlow

eee6abce6f090fa972095ea17e05fa17dd21f392
Meng Li <lm2343635@126.com>
2026-09-11 00:31:38 +0800

    Use AnyObject for event binder protocol

--- RxController/Classes/RxControllerEventBinder.swift
+++ RxController/Classes/RxControllerEventBinder.swift
@@ -26,7 +26,7 @@
 import RxCocoa
 import RxSwift
 
-public protocol RxControllerEventBinder: class {
+public protocol RxControllerEventBinder: AnyObject {
     var events: PublishRelay<RxControllerEvent> { get }
     var parentEvents: PublishRelay<RxControllerEvent> { get }
     var disposeBag: DisposeBag { get }
